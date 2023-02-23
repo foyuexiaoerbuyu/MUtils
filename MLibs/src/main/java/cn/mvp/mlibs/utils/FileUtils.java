e92d478dad7996da176d0678b7aac6dd26bc6ec4
米伟强 <1950053842@qq.com>
2023-02-23 15:45:56 +0800

    图片大小

--- MLibs/src/main/java/cn/mvp/mlibs/utils/FileUtils.java
+++ MLibs/src/main/java/cn/mvp/mlibs/utils/FileUtils.java
@@ -16,6 +16,7 @@ import java.io.OutputStream;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.text.DecimalFormat;
 
 import cn.mvp.mlibs.log.XLogUtil;
 
@@ -564,4 +565,28 @@ public class FileUtils {
         return false;
     }
 
+    /**
+     * @return 文件大小
+     */
+    public static String getSize(long size) {
+        //获取到的size为：1705230
+        int GB = 1024 * 1024 * 1024;//定义GB的计算常量
+        int MB = 1024 * 1024;//定义MB的计算常量
+        int KB = 1024;//定义KB的计算常量
+        DecimalFormat df = new DecimalFormat("0.00");//格式化小数
+        String resultSize = "0";
+        if (size / GB >= 1) {
+            //如果当前Byte的值大于等于1GB
+            resultSize = df.format(size / (float) GB) + "GB   ";
+        } else if (size / MB >= 1) {
+            //如果当前Byte的值大于等于1MB
+            resultSize = df.format(size / (float) MB) + "MB   ";
+        } else if (size / KB >= 1) {
+            //如果当前Byte的值大于等于1KB
+            resultSize = df.format(size / (float) KB) + "KB   ";
+        } else {
+            resultSize = size + "B   ";
+        }
+        return resultSize;
+    }
 }
