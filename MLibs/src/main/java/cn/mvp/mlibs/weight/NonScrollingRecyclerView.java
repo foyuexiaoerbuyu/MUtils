aacc63f222813823d5ad40f300245114a1e6c0f8
米伟强 <1950053842@qq.com>
2023-06-16 11:05:48 +0800

    修改兼容版全部代码为AndroidX版

--- MLibs/src/main/java/cn/mvp/mlibs/weight/NonScrollingRecyclerView.java
+++ MLibs/src/main/java/cn/mvp/mlibs/weight/NonScrollingRecyclerView.java
@@ -1,34 +1,36 @@
-//package cn.mvp.mlibs.weight;
-//
-//import android.content.Context;
-//import android.util.AttributeSet;
-//import android.view.MotionEvent;
-//
-///**
-// * 禁止滑动的RecyclerView
-// * android:scrollbars="none"
-// */
-//public class NonScrollingRecyclerView extends RecyclerView {
-//
-//    public NonScrollingRecyclerView(Context context) {
-//        super(context);
-//    }
-//
-//    public NonScrollingRecyclerView(Context context, AttributeSet attrs) {
-//        super(context, attrs);
-//    }
-//
-//    public NonScrollingRecyclerView(Context context, AttributeSet attrs, int defStyle) {
-//        super(context, attrs, defStyle);
-//    }
-//
-//    @Override
-//    public boolean onTouchEvent(MotionEvent e) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean onInterceptTouchEvent(MotionEvent e) {
-//        return false;
-//    }
-//}
+package cn.mvp.mlibs.weight;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * 禁止滑动的RecyclerView
+ * android:scrollbars="none"
+ */
+public class NonScrollingRecyclerView extends RecyclerView {
+
+    public NonScrollingRecyclerView(Context context) {
+        super(context);
+    }
+
+    public NonScrollingRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NonScrollingRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent e) {
+        return false;
+    }
+}
